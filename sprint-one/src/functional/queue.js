6a8255dcaca06657e914c1799db999147b6aaeac
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-07-18 18:57:02 -0500

    add initial commit with functional stack, and queue completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,106 @@
 var Queue = function() {
-  var someInstance = {};
+ var someInstance = {};
 
-  // Use an object with numeric keys to store values
-  var storage = {};
+ // Use an object with numeric keys to store values
+ var storage = {};
+ var start = 1;
+ var last = 1;
+ // Implement the methods below
 
-  // Implement the methods below
+ someInstance.enqueue = function(value) {
+    // storage[Object.keys(storage).length] = value;
+    storage[start] = value;
+    start++;
+ };
 
-  someInstance.enqueue = function(value) {
-  };
+ someInstance.dequeue = function() {
+   // if (someInstance.size < 1) {
+   //   return undefined;
+   // }
 
-  someInstance.dequeue = function() {
-  };
+   // last = Object.keys(storage).length - 1;
 
-  someInstance.size = function() {
-  };
+   var dequeued = storage[last];
+   delete storage[last];
+   last++;
+   return dequeued;
+ };
 
-  return someInstance;
+ someInstance.size = function() {
+   // if(Object.keys(storage).length < 1){
+   //   return 0;
+   // }
+
+   if (start - last < 0) {
+     return 0;
+   }
+   return start - last;
+
+    // return Object.keys(storage).length;
+
+ };
+
+ return someInstance;
 };
+
+// var Queue = function() {
+//   var someInstance = {};
+//
+// // Use an object with numeric keys to store values
+//   var storage = {};
+//   var count = 0;
+//
+// // Implement the methods below
+//
+// someInstance.enqueue = function(value) {
+//     storage[count]=value;
+//     count ++;
+//   };
+//
+// someInstance.dequeue = function() {
+//     if ( count < 1 ){
+//       return ;
+//     }
+//     var popedValue = storage[0];
+//     delete storage[0];
+//     for ( var i = 1 ; i < count ; i ++){
+//       storage[i-1] = storage[i];
+//     }
+//     delete storage[count-1];
+//     count --;
+//     return popedValue;
+//   };
+//   someInstance.size = function() {
+//     return count;
+//   };
+//   return someInstance;
+// };
+
+// var Queue = function() {
+//   var someInstance = {};
+//
+//   // Use an object with numeric keys to store values
+//   var storage = {};
+//   var head = 0
+//   var tail = 0
+//
+//   // Implement the methods below
+//
+//   someInstance.enqueue = function(value) {
+//     storage[tail] = value;
+//     tail+=1;
+//   };
+//
+//   someInstance.dequeue = function() {
+//   	var val = storage[head];
+//     delete storage[head]
+//     head+=1;
+//     return val;
+//   };
+//
+//   someInstance.size = function() {
+//     return Object.keys(storage).length;
+//   };
+//
+//   return someInstance;
+// };
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,32 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size()] = value;
+
+    // storage[Object.keys(storage).length + 1] = value;
+    // return storage;
+
   };
 
   someInstance.pop = function() {
+    var popVal = storage[someInstance.size() - 1];
+    delete storage[someInstance.size() - 1];
+    return popVal;
+
+    // var popVal = storage[Object.keys(storage)[Object.keys(storage).length - 1]];
+    // delete storage[Object.keys(storage)[Object.keys(storage).length - 1]];
+
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
+
+    // if (Object.keys(storage).length === 0) {
+    //   return 0;
+    // } else {
+    //     return Object.keys(storage).length - 1;
+    //   }
+
   };
 
   return someInstance;
