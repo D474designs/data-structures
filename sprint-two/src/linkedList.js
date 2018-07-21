818a86d46b3e6e42f02a7a996cb08f868739d045
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-07-20 19:48:25 -0500

    add completed linkedList problem

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,34 +1,95 @@
-var LinkedList = function() {
-  var list = {};
-  list.head = null;
-  list.tail = null;
-
-  list.addToTail = function(value) {
-     this.tail = Node(value);
-  };
-
-  list.removeHead = function() {
-    var garfield = this.head;
-    delete this.head;
-    this.head = node.next;
-    return garfield;
-  };
-
-  list.contains = function(target) {
-  };
-
-  return list;
-};
-
-var Node = function(value) {
-  var node = {};
-
-  node.value = value;
-  node.next = null;
-
-  return node;
-};
-
 /*
  * Complexity: What is the time complexity of the above functions?
+
+ addToTail is constant time
+ removeHead is constant time
+ contains is linear time
+
  */
+
+ var LinkedList = function() {
+   var list = {};
+   list.head = null;
+   list.tail = null;
+
+   list.addToTail = function(value) {
+
+     var node = new Node(value);
+     if (this.head === null) {
+       this.head = node;
+       this.tail = node;
+     } else {
+       this.tail.next = node;
+       this.tail = node;
+     }
+
+   };
+
+   list.removeHead = function() {
+     var babyWalrus = this.head.value;
+     this.head = this.head.next;
+     return babyWalrus;
+
+   };
+
+   list.contains = function(target) {
+     var currentHead = this.head;
+     while (currentHead) {
+       if (target === currentHead.value) {
+         return true;
+       } else {
+         currentHead = currentHead.next;
+       }
+     }
+     return false;
+
+   };
+
+   return list;
+
+ };
+
+ var Node = function(value) {
+   var node = {};
+
+   node.value = value;
+   node.next = null;
+
+   return node;
+   
+ };
+
+ // var LinkedList = function() {
+ //   var list = {};
+ //   list.head = null;
+ //   list.tail = null;
+ //
+ //   list.addToTail = function(value) {
+ //
+ //      // this.tail = Node(value);
+ //
+ //   };
+ //
+ //   list.removeHead = function() {
+ //
+ //     // if (!this.head) {
+ //     //   return null;
+ //     // }
+ //     // let value = this.head.value;
+ //
+ //   };
+ //
+ //   list.contains = function(target) {
+ //   };
+ //
+ //   return list;
+ // };
+ //
+ // var Node = function(value) {
+ //   var node = {};
+ //
+ //   node.value = value;
+ //   node.next = null;
+ //
+ //   return node;
+ // };
